4138e920de06bc9690699aa03522567dfacf5fa8
Alex Berliner <alexberliner@gmail.com>
2022-01-06 10:30:09 -0500

    Description, clean

--- target/debug/deps/windback.d
+++
@@ -1,5 +0,0 @@
-C:\Users\alexb\Code\windback\target\debug\deps\windback.exe: src\main.rs
-
-C:\Users\alexb\Code\windback\target\debug\deps\windback.d: src\main.rs
-
-src\main.rs:
--- target/debug/windback.d
+++
@@ -1,1 +0,0 @@
-C:\Users\alexb\Code\windback\target\debug\windback.exe: C:\Users\alexb\Code\windback\src\main.rs
